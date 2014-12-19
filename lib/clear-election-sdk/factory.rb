06fd5667ac6b304d66deedfe85e3a714d3e8f9bc
ronen barzel <ronen@barzel.org>
2014-12-19 13:38:51 +0000

    add more options for invalid ballot:  :contestId  :writeIn  :duplicateChoice  :multiplicityTooFew  :multiplicityTooMany  :ranking
    (invalid :writeIn also required adding writeIn option for election factory to disallow writeins)

--- lib/clear-election-sdk/factory.rb
+++ lib/clear-election-sdk/factory.rb
@@ -17,7 +17,8 @@ module ClearElection
       signin: nil,
       booth: nil,
       pollsOpen: nil,
-      pollsClose: nil
+      pollsClose: nil,
+      writeIn: true
     )
       one_month = 60*60*24*30
       Election.new(
@@ -27,37 +28,46 @@ module ClearElection
         pollsOpen: (pollsOpen || Time.now - one_month).to_datetime(),
         pollsClose: (pollsClose || Time.now + one_month).to_datetime(),
         contests: [
-          Election::Contest.new(
-            contestId: seq(:contestId),
-            name: seq("Contest Name"),
-            ranked: true,
-            multiplicity: 3,
-            writeIn: true,
-            candidates: 3.times.map{ Election::Candidate.new(candidateId: seq(:candidateId), name: seq("Candidate Name")) }
-          ),
-          Election::Contest.new(
-            contestId: seq(:contestId),
-            name: seq("Contest Name"),
-            candidates: 2.times.map{ Election::Candidate.new(candidateId: seq(:candidateId), name: seq("Candidate Name")) }
-          ),
+          Factory.contest(ranked: true, multiplicity: 3, writeIn: writeIn, ncandidates: 3),
+          Factory.contest(ncandidates: 2)
         ]
       )
     end
 
-    def self.ballot(election=nil, ballotId: nil, uniquifier: nil, demographic: nil, invalid: nil)
+    def self.contest(ranked: nil, multiplicity: nil, writeIn: nil, ncandidates: 3)
+      Election::Contest.new(
+        contestId: seq(:contestId),
+        name: seq("Contest Name"),
+        ranked: ranked,
+        multiplicity: multiplicity,
+        writeIn: writeIn,
+        candidates: ncandidates.times.map{ Election::Candidate.new(candidateId: seq(:candidateId), name: seq("Candidate Name")) }
+      )
+    end
+
+    def self.ballot(election=nil, ballotId: nil, uniquifier: nil, demographic: nil, invalid: nil, complete: true)
       election ||= self.election
+      contests = election.contests.dup
+      contests = contests.drop(1) if not complete
+      contests << Factory.contest if invalid == :contestId
       Ballot.new(
         ballotId: ballotId || seq(:ballotId),
         uniquifier: uniquifier || seq(:uniquifier),
-        contests: election.contests.map { |contest|
+        contests: contests.map { |contest|
           options = contest.candidates.map(&:candidateId)
           options << "ABSTAIN"
           options << "WRITEIN: TestWritein" if contest.writeIn
           options.shuffle!
           options.push "Test-Invalid-CandidateId" if invalid == :candidateId
+          options.push "WRITEIN: Test-Unpermitted-Writein" if invalid == :writeIn and not contest.writeIn
+          options.push options.last if invalid == :duplicateChoice
+          nchoices = contest.multiplicity
+          nchoices -= 1 if invalid == :multiplicityTooFew
+          nchoices += 1 if invalid == :multiplicityTooMany
           Ballot::Contest.new(
             contestId: contest.contestId,
-            choices: contest.multiplicity.times.map {|rank|
+            choices: nchoices.times.map {|rank|
+              rank += 1 if invalid == :ranking
               Ballot::Choice.new(candidateId: options.pop, rank: rank)
             }
           )
